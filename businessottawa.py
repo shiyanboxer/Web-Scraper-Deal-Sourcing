91dd649873842d8b5758f300b78ee80ceae8a7a0
Shiyan Boxer <shiyanboxer7@gmail.com>
2020-06-18 19:14:12 -0400

    Update businessottawa.py

--- businessottawa.py
+++ businessottawa.py
@@ -20,31 +20,34 @@ csv_writer.writerow(['Company', 'Address', 'Phone'])
 # *** CONTACT BLOCK ***
 containers = soup.find('div', {'class': 'gz-list-card-wrapper col-sm-6 col-md-4'})
 
-for container in containers:
- 
-    # print(len(containers)) - prints number of contacts
-    # print(containers.prettify()) - prints formated html
+# Only collect 10 contacts
+i = 0
+while i < 10: 
+ for container in containers:
 
-    # COMPANY
-    companyblock = containers.find('div',{'class': 'card-header'})
-    company = companyblock.find('a')['alt']
-    # print(company)
-     
-    # ADDRESS
-    addressphoneblock = containers.find('div', {'class': 'card-body gz-results-card-body'})
-    address = addressphoneblock.find('span', {'class': 'gz-street-address'}).getText()
-    # print(address)
+     # print(len(containers)) - prints number of contacts
+     # print(containers.prettify()) - prints formated html
 
-    # PHONE NUMBER
-    phoneblock = addressphoneblock.find('li', {'class': 'list-group-item gz-card-phone'})
-    p = phoneblock.find('a', 'card-link')['href']
-    phone = p.replace('tel:', '')
-    # print(phone)
-    
-    
-    # *** WRITE AND CLOSE CSV ***
-    # write the data to CSV for every iteration
-    csv_writer.writerow([company, address, phone])
+     # COMPANY
+     companyblock = containers.find('div',{'class': 'card-header'})
+     company = companyblock.find('a')['alt']
+     # print(company)
+
+     # ADDRESS
+     addressphoneblock = containers.find('div', {'class': 'card-body gz-results-card-body'})
+     address = addressphoneblock.find('span', {'class': 'gz-street-address'}).getText()
+     # print(address)
+
+     # PHONE NUMBER
+     phoneblock = addressphoneblock.find('li', {'class': 'list-group-item gz-card-phone'})
+     p = phoneblock.find('a', 'card-link')['href']
+     phone = p.replace('tel:', '')
+     # print(phone)
+
+
+     # *** WRITE AND CLOSE CSV ***
+     # write the data to CSV for every iteration
+     csv_writer.writerow([company, address, phone])
 
 # outside of the loop, close the file
 csv_file.close()
